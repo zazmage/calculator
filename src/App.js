ce00bfab57cf28ebac9a6e606c6ab2f7b66223f5
daniel <daniel.zapata.682@gmail.com>
2021-11-23 17:11:52 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row, Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Row>
+          <Form.Control
+            type="text"
+            placeholder="Readonly input here..."
+            readOnly
+          />
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="outline-danger">AC</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-dark">/</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-dark">X</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="outline-secondary">7</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-secondary">8</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-secondary">9</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-dark">-</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="outline-secondary">4</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-secondary">5</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-secondary">6</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-dark">+</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="outline-secondary">1</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-secondary">2</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-secondary">3</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-primary">=</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="outline-secondary">0</Button>
+          </Col>
+          <Col>
+            <Button variant="outline-secondary">.</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
